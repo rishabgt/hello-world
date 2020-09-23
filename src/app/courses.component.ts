2b6027281072d4fa7825bffc717a919cb1a1d2f0
Rishab <rishabgt@gmail.com>
2020-09-23 17:19:59 +0530

    inital

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'courses',
+    template : '<h3>This is course component</h3>' 
+})
+
+export class CoursesComponent{
+
+}
